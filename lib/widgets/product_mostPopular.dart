0a83219d5b2c84d01c3d231e1ff3d4050d9d1db4
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-09-28 23:15:24 +0800

    updated edgeInsets inmostpopular class

--- lib/widgets/product_mostPopular.dart
+++ lib/widgets/product_mostPopular.dart
@@ -5,15 +5,15 @@ class ProductMostpopular extends StatelessWidget {
 
   Widget _buildProductCard(String imagePath, String likes, String label) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(16),
         ),
         elevation: 3,
-        margin: const EdgeInsets.only(right: 12),
+        margin: const EdgeInsets.only(right: 0),
         child: Container(
-          width: 140,
+          width: 150,
           padding: const EdgeInsets.all(8),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,7 +22,7 @@ class ProductMostpopular extends StatelessWidget {
                 borderRadius: BorderRadius.circular(12),
                 child: Image.asset(
                   imagePath,
-                  height: 160,
+                  height: 140,
                   width: double.infinity,
                   fit: BoxFit.cover,
                 ),
@@ -30,7 +30,7 @@ class ProductMostpopular extends StatelessWidget {
               const SizedBox(height: 8),
 
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
                     likes,
@@ -40,16 +40,20 @@ class ProductMostpopular extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(width: 2),
-                  const Icon(Icons.favorite, color: Colors.blue, size: 18),
+                  const Icon(
+                    Icons.favorite,
+                    color: Color.fromARGB(238, 0, 76, 255),
+                    size: 18,
+                  ),
+                  const Spacer(),
+                  Text(
+                    label,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ],
-              ),
-              const SizedBox(height: 4),
-              Text(
-                label,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
               ),
             ],
           ),
@@ -78,16 +82,17 @@ class ProductMostpopular extends StatelessWidget {
                     "See All",
                     style: TextStyle(fontSize: 21, fontWeight: FontWeight.w600),
                   ),
-                  SizedBox(width: 4),
-                  Icon(
-                    Icons.arrow_forward,
-                    color: Color.fromARGB(255, 0, 126, 230),
+                  SizedBox(width: 12),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: const Color.fromARGB(255, 0, 126, 230),
+                    child: const Icon(Icons.arrow_forward, color: Colors.white),
                   ),
                 ],
               ),
             ],
           ),
-          const SizedBox(height: 12),
+          const SizedBox(height: 8),
 
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
